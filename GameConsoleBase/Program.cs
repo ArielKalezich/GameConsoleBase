db704a9fac613a043d2eba782efc7ea614d73d90
Owner <stam1@gmail.com>
2025-12-04 10:42:10 +0200

    Add project files.

---
+++ GameConsoleBase/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace GameConsoleBase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Interfaces.IGamePlay game = new Games.FluffyBirdGame();
+            game.Play();
+            game=new Games.TetrisGame();
+            game.Play();
+            game=new Games.PacManGame();
+            game.Play();
+		}
+    }
+}
